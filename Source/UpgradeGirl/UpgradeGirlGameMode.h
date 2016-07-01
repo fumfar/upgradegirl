f9e581fd0acf595ed3e5ffe1c99ba023d4dddfa3
fumfar <destroydelete@gmail.com>
2016-06-30 19:19:53 -0500

    Commit 2

---
+++ Source/UpgradeGirl/UpgradeGirlGameMode.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameMode.h"
+#include "UpgradeGirlGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UPGRADEGIRL_API AUpgradeGirlGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
